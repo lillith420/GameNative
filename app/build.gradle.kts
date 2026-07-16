4c269a63001422d71ca152e7511f077cb5ce3c04
Armani Esty <96528575+lillith420@users.noreply.github.com>
2026-07-16 18:45:29 -0400

    Remove STEAMGRIDDB_API_KEY from build.gradle.kts
    Removed unnecessary buildConfigField for STEAMGRIDDB_API_KEY.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,6 @@ android {
         buildConfigField("String", "POSTHOG_API_KEY", "\"phx_BHituEUbaDxeu9oeyKCajLqwjbcRiZYtTDQtbU85aCAzqUjr\"")
         buildConfigField("String", "POSTHOG_HOST", "\"https://us.i.posthog.com\"")
         buildConfigField("String", "STEAMGRIDDB_API_KEY", "\"65063399717227a51f34e00ffee55932\"")
-
         val iconValue = "@mipmap/ic_launcher"
         val iconRoundValue = "@mipmap/ic_launcher_round"
         manifestPlaceholders.putAll(
